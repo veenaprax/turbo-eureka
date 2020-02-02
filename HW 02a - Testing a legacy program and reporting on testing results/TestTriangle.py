e957212b577851e26835276464751327691f3fcc
veenaprax <veenaprax@users.noreply.github.com>
2020-02-01 22:50:45 -0500

    Corrected source code

--- HW 02a - Testing a legacy program and reporting on testing results/TestTriangle.py
+++ HW 02a - Testing a legacy program and reporting on testing results/TestTriangle.py
@@ -18,13 +18,23 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertIs(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+    def testRightTriangleB(self):
+        self.assertIs(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+
+    def testScaleneTriangles(self):
+        self.assertIs(classifyTriangle(5,4,6),'Scalene','5,4,6 should be Scalene')
         
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def testNotATriangles(self):
+        self.assertIs(classifyTriangle(1,10,12),'NotATriangle')
+
+    def testEquilateralTriangles(self):
+        self.assertIs(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsocelesTriangles(self):
+        self.assertIs(classifyTriangle(3, 4, 4), 'Isoceles','3,4,4 should be Isoceles')
+
 
 if __name__ == '__main__':
     print('Running unit tests')
--- HW 02a - Testing a legacy program and reporting on testing results/Triangle.py
+++ HW 02a - Testing a legacy program and reporting on testing results/Triangle.py
@@ -22,7 +22,7 @@ def classifyTriangle(a,b,c):
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
+        If the sum of any two sides equals the square of the third side, then return 'Right'
       
       BEWARE: there may be a bug or two in this code
     """
@@ -31,7 +31,7 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
@@ -43,15 +43,15 @@ def classifyTriangle(a,b,c):
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a > (b + c)) or (b > (a + c)) or (c > (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
     if a == b and b == a:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif (a * a + b * b == c * c) or (c * c + b * b == a * a) or (a * a + c * c == b * b):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and (b != c) and (a != b):
         return 'Scalene'
     else:
         return 'Isoceles'
